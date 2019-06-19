b2da334c75c6831333765f999dbdf82a0b50caac
Anand P S <49161738+anandvfcvijay@users.noreply.github.com>
2019-06-19 20:35:07 +0530

    Update index.php

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ require 'libs/Youtube.php';
 use Smoqadam\Telegram;
 use Smoqadam\Youtube;
 
-$api_token = 'API_TOKEN';
+$api_token = '888835542:AAEUkooaATEPTJcdXSzvy-m1tNLEa5AOUFU';
 
 $tg = new Telegram($api_token);
 $y = new Youtube();
